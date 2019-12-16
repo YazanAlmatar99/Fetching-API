f46bc0f1c31294d85ff36b6b6ef7da6bcb72a930
Yazan <yazanmatar90@gmail.com>
2019-12-16 17:33:51 -0500

    done

--- js/script.js
+++ js/script.js
@@ -1,42 +1,33 @@
 jQuery(document).ready(function(){
     // $('body').html('<img src="https://placehold.it/1000/1000">');
-    $('#books').html('<div class="loader center"></div>');
+    $('#movies').html('<div class="loader center"></div>');
 
     axios.get('http://csc225.mockable.io/movies')
         .then(function(response){
             console.log(response.data);
-            var booksHTML = response.data.map(function(book){
-                // console.log(book);
-                // return $('<p>')
-                //     .addClass('book')
-                //     .data('book', book.id)
-                //     .html(book.title);
-            return '<li class="book list-group-item" data-book="'+book.id+'">' + 
-                book.title + '</li>' ;
+            var moviesHTML = response.data.map(function(movie){
+              
+            return '<li class="movie list-group-item" data-movie="'+movie.id+'">' + 
+                movie.title + '</li>' ;
             });
-            // console.log(booksHTML);
-            $('#books').html(booksHTML);
+            $('#movies').html(moviesHTML);
 
 
         });
 
-        $('body').on('click', '.book', function(){
-            var id = $(this).data('book');
+        $('body').on('click', '.movie', function(){
+            var id = $(this).data('movie');
             var url = 'http://csc225.mockable.io/movies/' + id;
-
+            $('#current-movie').html('<div class="loader center"></div>');
             axios.get(url).then(function(response){
-                var book = response.data;
-                var bookHTML = '<p>' + book.author + '</p>';
-                bookHTML += '<p>' + book.title + '</p>';
-                bookHTML += '<p>' + book.language + '</p>';
-                bookHTML += '<a href="' + book.link + '">[LINK]</a>';
-                $('#current-book').html(bookHTML);
+                var movie = response.data;
+                var movieHTML = '<p>Titel: ' + movie.title + '</p>';
+                movieHTML += '<p> Director: ' + movie.director + '</p>';
+                movieHTML += '<p> Release: ' + (movie.release ? movie.release : "N/A") + '</p>';
+                movieHTML += '<img class="poster" src="' + movie.poster + '">';
+                $('#current-movie').html(movieHTML);
                 
             })
 
         });
-
-
-
-        // console.log('i come after the request');
 });
